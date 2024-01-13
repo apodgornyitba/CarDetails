39ecb046e50f1eb535bd537e2e0654fd62553cae
apodgornyitba <apodgorny@itba.edu.ar>
2024-01-12 23:21:07 -0300

    Feat: Final styling details. Adding functionality to the TextFields.
    Signed-off-by: apodgornyitba <apodgorny@itba.edu.ar>

--- src/App.js
+++ src/App.js
@@ -13,6 +13,60 @@ function App() {
     const [expirationDate, setExpirationDate] = useState('00/00');
     const [cvv, setCvv] = useState('000');
 
+    const [cardNumberError, setCardNumberError] = useState('');
+    const [expMonthError, setExpMonthError] = useState('');
+    const [expYearError, setExpYearError] = useState('');
+    const [cvcError, setCvcError] = useState('');
+
+    const validateCardNumber = (value) => {
+        const isValid = /^\d{16}$/.test(value);
+        setCardNumberError(isValid ? '' : 'Wrong format');
+        return isValid;
+    };
+
+    const validateExpMonth = (value) => {
+        const isValid = value >= 1 && value <= 12;
+        setExpMonthError(isValid ? '' : 'Invalid month');
+        return isValid;
+    };
+
+    const validateExpYear = (value) => {
+        const isValid = value >= 0 && value <= 99;
+        setExpYearError(isValid ? '' : 'Invalid year');
+        return isValid;
+    };
+
+    const validateCvc = (value) => {
+        const isValid = /^\d{3}$/.test(value);
+        setCvcError(isValid ? '' : 'Invalid CVC');
+        return isValid;
+    };
+
+    const handleCardNumberChange = (event) => {
+        const value = event.target.value;
+        setCardNumber(value);
+        validateCardNumber(value);
+    };
+
+    const handleExpMonthChange = (event) => {
+        const value = parseInt(event.target.value, 10);
+        setExpirationDate(`${value <= 9 ? '0' : ''}${value}/${expirationDate.split('/')[1]}`);
+        validateExpMonth(value);
+    };
+
+    const handleExpYearChange = (event) => {
+        const value = parseInt(event.target.value, 10);
+        setExpirationDate(`${expirationDate.split('/')[0]}/${value <= 9 ? '0' : ''}${value}`);
+        validateExpYear(value);
+    };
+
+    const handleCvcChange = (event) => {
+        const value = event.target.value;
+        setCvv(value);
+        validateCvc(value);
+    };
+
+
     const leftContainerStyle = {
         backgroundImage: `url(${BGMain})`,
         backgroundRepeat: 'no-repeat',
@@ -23,9 +77,9 @@ function App() {
     };
 
     const rightContainerStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-
+        display: 'grid',
+        alignContent: 'center',
+        marginLeft: '10%',
     };
 
     const CardWithBG = (props) => {
@@ -33,10 +87,11 @@ function App() {
             backgroundImage: `url(${props.imageUrl})`,
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'auto',
-            width: '447px',
-            height: '245px',
-            boxShadow: 'none',
-            backgroundColor: 'inherit',
+            width: '446px',
+            height: '244px',
+            borderRadius: '10px',
+            boxShadow: '3px 3px 25px 3px rgba(0, 0, 0, 0.2)',
+            backgroundColor: 'transparent',
             marginLeft: '25%',
             marginBottom: '5%',
         };
@@ -51,19 +106,29 @@ function App() {
             boxShadow: 'none',
             backgroundColor: 'inherit',
             width: props.width,
+            marginRight: props.marginRight,
 
         };
 
         return <TextField className="textField" placeholder={props.placeholder} style={textFieldStyle}
                           sx={{
-                              '& .MuiOutlinedInput-root': {
-                                  fontFamily:'inherit',
-                                  borderRadius: '10px',
 
+                              '& .MuiOutlinedInput-root': {
+                                  fontFamily: 'inherit',
+                                  borderRadius: '7.5px',
                               },
                               '& .MuiOutlinedInput-input': {
-                                  height: props.height,
+                                  height: '10px',
                               },
+                              '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+                                  borderRadius: '7.5px',
+                                  border: '2px solid transparent',
+                                  background: 'linear-gradient(to right, hsl(249, 99%, 64%), hsl(278, 94%, 30%)) border-box',
+                                  WebkitMask:
+                                      'linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0)',
+                                  WebkitMaskComposite: 'xor',
+                                  maskComposite: 'exclude',
+                          },
                           }}/>;
     };
 
@@ -95,18 +160,23 @@ function App() {
                 </div>
             </Container>
             <Container className="rigthContainer" style={rightContainerStyle}>
-                <Card className="content">
+                <Card className="content"
+                      sx={{
+                          boxShadow: 'none',
+                      }}
+                >
                     <p>CARDHOLDER NAME</p>
                     <CustomTextField
                         placeholder="e.g. Jane Appleseed"
-                        width="40%"
-                        height="15px"
+                        width="65%"
                     />
                     <p>CARD NUMBER</p>
                     <CustomTextField
                         placeholder="e.g. 1234 5678 9123 0000"
-                        width='40%'
-                        height="15px"
+                        width='65%'
+                        onChange={handleCardNumberChange}
+                        error={cardNumberError !== ''}
+                        helperText={cardNumberError}
                     />
                     <div className="expAndCvc">
                         <div className="expDate">
@@ -114,13 +184,19 @@ function App() {
                             <div className="expDateFields">
                                 <CustomTextField
                                     placeholder="MM"
-                                    height="15px"
-                                    width="45%"
+                                    width="40%"
+                                    marginRight="5%"
+                                    onChange={handleExpMonthChange}
+                                    error={expMonthError !== ''}
+                                    helperText={expMonthError}
                                 />
                                 <CustomTextField
                                     placeholder="YY"
-                                    height="15px"
-                                    width="45%"
+                                    width="40%"
+                                    marginRight="5%"
+                                    onChange={handleExpYearChange}
+                                    error={expYearError !== ''}
+                                    helperText={expYearError}
                                 />
                             </div>
                         </div>
@@ -128,13 +204,29 @@ function App() {
                             <p>CVC</p>
                             <CustomTextField
                                 placeholder="e.g. 123"
-                                height="15px"
-
+                                width="190%"
+                                onChange={handleCvcChange}
+                                error={cvcError !== ''}
+                                helperText={cvcError}
                             />
                         </div>
                     </div>
-                    <div>
-                        <Button>CONFIRM</Button>
+                    <div className="confirmButton">
+                        <Button
+                            sx={{
+                                backgroundColor: 'hsl(278, 68%, 11%)',
+                                fontFamily: 'inherit',
+                                color: 'hsl(270, 3%, 87%)',
+                                width: '65%',
+                                height: '50px',
+                                borderRadius: '7.5px',
+                                textTransform: 'none',
+                                marginTop: '5%',
+                                '&:hover': {
+                                    backgroundColor: 'hsl(279, 6%, 55%)',
+                                },
+                            }}
+                        >Confirm</Button>
                     </div>
                 </Card>
             </Container>
